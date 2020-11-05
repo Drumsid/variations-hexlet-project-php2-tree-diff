b56f5ba7e47bb2fcd0a15b12a97223ca2ed52539
densidnay@gmail.com <Jesuschrist05882>
2020-11-05 17:21:05 +0300

    add version 2

---
+++ version2/differ.php
@@ -0,0 +1,57 @@
+<?php
+
+$beforeTree = '{
+  "host": "hexlet.io",
+  "timeout": 50,
+  "proxy": "123.234.53.22",
+  "follow": false
+}';
+$afterTree = '{
+  "timeout": 20,
+  "verbose": true,
+  "host": "hexlet.io"
+}';
+
+$beforeTree = json_decode($beforeTree);
+$afterTree = json_decode($afterTree);
+
+// print_r($objTree);
+
+function test($tree)
+{
+  $res = [];
+
+  foreach($tree as $key => $val) {
+    if (is_object($val)) {
+      $res[] = ['name' => $key, /*'value' => $val,*/ 'meta' => [], 'type' => 'dir', 'children' => test($val)]; 
+    }
+    else {
+      $res[] = ['name' => $key, 'meta' => ['value' => boolOrNullToString($val)], 'type' => 'file'];
+    }
+  }
+  return $res;
+}
+
+function boolOrNullToString($data)
+{
+    if (is_null($data)) {
+        return 'null';
+    }
+    if (is_bool($data) && $data === true) {
+        return 'true';
+    }
+    if (is_bool($data) && $data === false) {
+        return 'false';
+    }
+    return $data;
+}
+$beforeTree = test($beforeTree);
+$afterTree = test($afterTree);
+
+// print_r($beforeTree);
+// print_r($afterTree);
+
+function differ($beforeTree, $afterTree)
+{
+  
+}
---
+++ version2/test.php
@@ -0,0 +1,69 @@
+<?php
+
+$tree = '{
+  "host": "hexlet.io",
+  "timeout": 50,
+  "proxy": "123.234.53.22",
+  "follow": false
+}';
+
+$deepTree = '{
+  "common": {
+    "setting1": "Value 1",
+    "setting2": 200,
+    "setting3": true,
+    "setting6": {
+      "key": "value",
+      "doge": {
+        "wow": ""
+      }
+    }
+  },
+  "group1": {
+    "baz": "bas",
+    "foo": "bar",
+    "nest": {
+      "key": "value"
+    }
+  },
+  "group2": {
+    "abc": 12345,
+    "deep": {
+      "id": 45
+    }
+  }
+}';
+
+$objTree = json_decode($tree);
+
+// print_r($objTree);
+
+function test($tree)
+{
+  $res = [];
+
+  foreach($tree as $key => $val) {
+    if (is_object($val)) {
+      $res[] = ['name' => $key, /*'value' => $val,*/ 'meta' => [], 'type' => 'dir', 'children' => test($val)]; 
+    }
+    else {
+      $res[] = ['name' => $key, 'meta' => ['value' => boolOrNullToString($val)], 'type' => 'file'];
+    }
+  }
+  return $res;
+}
+
+function boolOrNullToString($data)
+{
+    if (is_null($data)) {
+        return 'null';
+    }
+    if (is_bool($data) && $data === true) {
+        return 'true';
+    }
+    if (is_bool($data) && $data === false) {
+        return 'false';
+    }
+    return $data;
+}
+print_r(test($objTree));
